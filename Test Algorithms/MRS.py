eb77190b58f2d76df2c948aa97057dc7cffad924
tasos-a <73363953+tasos-a@users.noreply.github.com>
2022-06-25 10:08:29 +0100

    Add files via upload

---
+++ Test Algorithms/MRS.py
@@ -0,0 +1,85 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.factory import get_problem
+from time import sleep
+from pythonosc import udp_client
+import numpy as np
+import matplotlib.pyplot as plt
+from pymoo.factory import get_performance_indicator
+from pymoo.visualization.scatter import Scatter
+from sklearn.preprocessing import MinMaxScaler
+
+        
+class MultiObjectiveRandomSearch(NSGA2):
+
+    def _infill(self):
+        return self.initialization.sampling.do(self.problem, self.pop_size)
+
+
+problem = get_problem("zdt1")
+
+algorithm = MultiObjectiveRandomSearch(pop_size=100)
+
+hv = get_performance_indicator("hv", ref_point=np.array([1.2,1.2]))
+
+# prepare the algorithm to solve the specific problem (same arguments as for the minimize function)
+algorithm.setup(problem, ('n_gen', 250), seed=1)
+sleep(0)
+
+# until the algorithm has no terminated
+while algorithm.has_next():
+    
+    # do the next iteration
+    algorithm.next()
+
+    # do same more things, printing, logging, storing or even modifying the algorithm object
+    #print(algorithm.n_gen, algorithm.evaluator.n_eval)
+    
+    approximation_set = algorithm.result().F
+    
+    #sort the approximation set so that left->right means min(x)->max(x) and max(y)->min(y)
+    approximation_set = approximation_set[approximation_set[:,0].argsort()] 
+    
+    #calculate the hypervolume index
+    #first scale the approximation set so it fits the 0, 1 range
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_set = scaler.fit(approximation_set)
+    scaled_set = scaler.transform(approximation_set)
+    hypervolume = hv.do(scaled_set)
+    
+    #reset the hypervolume contributions
+    hypervolume_contributions_list = []
+    
+    #calculate the hypervolume contributions
+    for index in range(len(scaled_set)):
+        new_set = np.delete(scaled_set, index, 0) 
+        hypervolume_of_new_set = hv.do(new_set)
+        hypervolume_contribution = hypervolume - hypervolume_of_new_set 
+        hypervolume_contributions_list = np.array(hypervolume_contributions_list, dtype=np.float)
+        hypervolume_contributions_list = np.append(hypervolume_contributions_list, hypervolume_contribution)
+            
+    #convert the arrays into lists
+    #first the approximation set list
+    obj_one = ['{:f}'.format(item) for item in approximation_set[:, 0]]
+    obj_two = ['{:f}'.format(item) for item in approximation_set[:, 1]]
+    objs_combined = [obj_one, obj_two]
+    formatted_approximation_set = [' '.join(str(item) for item in column) for column in zip(*objs_combined)]
+    #then the hypervolume contributions list
+    formatted_hypervolume_contributions_list = ['{:f}'.format(item) for item in hypervolume_contributions_list]
+      
+    #send the results to SonOpt
+    client_a = udp_client.SimpleUDPClient("127.0.0.1", 5002)
+    client_a.send_message("start", formatted_approximation_set)
+    client_a = udp_client.SimpleUDPClient("127.0.0.1", 5003)
+    client_a.send_message("start", formatted_hypervolume_contributions_list)
+    
+    sleep(0.5)
+    
+res = approximation_set
+csfont = {'fontname':'Adobe Garamond pro'}
+plt.figure(figsize=(7, 5), linewidth=20)
+plt.scatter(res[:, 0], res[:, 1], s=50, facecolors='black', edgecolors='red', linewidths=2)
+plt.title("Objective Space", fontsize=20, **csfont)
+plt.show()
---
+++ Test Algorithms/Message formatting and OSC setup.py
@@ -0,0 +1,23 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+from time import sleep
+from pythonosc import udp_client
+
+######### format the approximation set and send it over to SonOpt via OSC #######
+#convert the arrays into lists
+#first the approximation set list
+obj_one = ['{:f}'.format(item) for item in approximation_set[:, 0]]
+obj_two = ['{:f}'.format(item) for item in approximation_set[:, 1]]
+objs_combined = [obj_one, obj_two]
+formatted_approximation_set = [' '.join(str(item) for item in column) for column in zip(*objs_combined)]
+#then the hypervolume contributions list
+formatted_hypervolume_contributions_list = ['{:f}'.format(item) for item in hypervolume_contributions_list]
+      
+#send the results to SonOpt
+client_a = udp_client.SimpleUDPClient("127.0.0.1", 5002)
+client_a.send_message("start", formatted_approximation_set)
+client_a = udp_client.SimpleUDPClient("127.0.0.1", 5003)
+client_a.send_message("start", formatted_hypervolume_contributions_list)
+    
+sleep(0.5)
