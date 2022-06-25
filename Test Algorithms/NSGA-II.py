e30a80a07255116ce9d7fb1ea3ce5ee938266caa
tasos-a <73363953+tasos-a@users.noreply.github.com>
2022-06-25 10:09:49 +0100

    Update NSGA-II.py

--- Test Algorithms/NSGA-II.py
+++ Test Algorithms/NSGA-II.py
@@ -1,6 +1,3 @@
-Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
 from pymoo.algorithms.moo.nsga2 import NSGA2
 from pymoo.factory import get_problem
 from time import sleep
